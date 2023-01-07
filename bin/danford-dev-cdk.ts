cc7b377fe2e72a871bf85db1ada62d42e76bf8ca
Dan Ford <dan@danford.dev>
2023-01-07 03:34:42 -0800

    Adding Live stack

--- bin/danford-dev-cdk.ts
+++ bin/danford-dev-cdk.ts
@@ -10,3 +10,9 @@ new DanfordDevCdkStack(app, 'DanFordDevTestStack', {
   siteDomain: 'test.danford.dev',
   certificateArn: 'arn:aws:acm:us-east-1:797251479904:certificate/afabbae4-72b0-4161-a216-9417b0891c68',
 });
+
+new DanfordDevCdkStack(app, 'DanFordDevLiveStack', {
+  env: { account:'480228180912', region: 'us-west-2' },
+  siteDomain: 'danford.dev',
+  certificateArn: 'arn:aws:acm:us-east-1:480228180912:certificate/6b5b3800-2f52-423b-a5be-a6270c4c97ad',
+});
